1e59111e962ed330d623f22431d664c8ebef4c11
masonorovic <lukashornych@protomail.com>
2021-03-18 08:29:08 +0100

    fixed foreign keys in ddl

--- docker/db/ddl.sql
+++ docker/db/ddl.sql
@@ -17,49 +17,59 @@ CREATE TABLE account (
     name VARCHAR(100) NOT NULL,
     passwordHash VARCHAR(512) NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    role VARCHAR(50) NOT NULL DEFAULT 'USER' REFERENCES role(code)
+    role VARCHAR(50) NOT NULL DEFAULT 'USER',
+    constraint cnfk_account_code foreign key (role) REFERENCES role (code)
 );
 
 CREATE TABLE channel (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     description VARCHAR(512),
-    type VARCHAR(50) not null references channelType (code)
+    type VARCHAR(50) not null,
+    constraint cnfk_channel_channelType foreign key (type) references channelType (code)
 );
 
 CREATE TABLE accountInChannel (
     channelId INT NOT NULL,
     accountId INT NOT NULL,
-    PRIMARY KEY (channelId, accountId)
+    PRIMARY KEY (channelId, accountId),
+    constraint cnfk_accountInChannel_account foreign key (accountId) references account (id),
+    constraint cnfk_accountInChannel_channel foreign key (channelId) references channel (id)
 );
 
 CREATE TABLE registrationInvitation (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     code VARCHAR(50) NOT NULL,
-    accountId INT REFERENCES account (id),
-    accepted BOOLEAN NOT NULL DEFAULT FALSE
+    accountId INT,
+    accepted BOOLEAN NOT NULL DEFAULT FALSE,
+    constraint cnfk_registrationInvitation_account foreign key (accountId) REFERENCES account (id)
 );
 
 CREATE TABLE channelInvitation (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    channelId INT NOT NULL REFERENCES channel(id),
-    accountId INT NOT NULL REFERENCES account(id),
-    accepted BOOLEAN NOT NULL DEFAULT FALSE
+    channelId INT NOT NULL,
+    accountId INT NOT NULL,
+    accepted BOOLEAN NOT NULL DEFAULT FALSE,
+    constraint cnfk_channelInvitation_channel foreign key (channelId) REFERENCES channel (id),
+    constraint cnfk_channelInvitation_account foreign key (accountId) REFERENCES account (id)
 );
 
 CREATE TABLE thread (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    channelId INT NOT NULL REFERENCES channel(id),
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    channelId INT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    constraint cnfk_thread_channel foreign key (channelId) references channel (id)
 );
 
 CREATE TABLE message (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    threadId INT NOT NULL REFERENCES thread(id),
-    creatorId INT NOT NULL REFERENCES account(id),
+    threadId INT NOT NULL,
+    creatorId INT NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    content VARCHAR(2048) NOT NULL
+    content VARCHAR(2048) NOT NULL,
+    constraint cnfk_message_thread foreign key (threadId) references thread (id),
+    constraint cnfk_message_creator foreign key (creatorId) references account (id)
 );
 
 
