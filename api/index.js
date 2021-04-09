b6122b776dc89531f4859cfcd207fc9bd849fb1a
masonorovic <lukashornych@protomail.com>
2021-04-09 16:47:19 +0200

    added uploading of user photo

--- api/index.js
+++ api/index.js
@@ -27,8 +27,8 @@ const server = http.createServer(app);
 
 // Express use parsers
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb', extended: true}));
 
 // Require API routes
 const accounts = require('./routes/accounts');
